8eeeaeb32263492b5522ac5195b9030db2f63e67
RainyF0rest <107770951+RainyF0rest@users.noreply.github.com>
2024-04-16 12:12:31 +0800

    netB

--- index.js
+++ index.js
@@ -6,4 +6,23 @@ function updatemenu() {
       document.getElementById('menu').style.borderRadius = '12px';
     }
   }
+
+
+  function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
   
